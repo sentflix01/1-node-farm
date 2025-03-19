7a5b06cc914001154fc2cc8d568a6121cafae701
Sintayehu <sentflix@gmail.com>
2025-03-19 10:04:41 +0300

    sync vs async

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const fs = require('fs')
 
-
+// Blocking, synchronous way
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
 
 const textOut = `this is what we know about the avocado; ${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt',textOut);
 console.log('File written!');
+
+// Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+  console.log(data);
+})
