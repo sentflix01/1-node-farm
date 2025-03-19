b2f4c69e9445ee2bc820a21f3eef2f90c1700072
Sintayehu <sentflix@gmail.com>
2025-03-19 10:17:42 +0300

    asynch readfile

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const fs = require('fs')
 
 // Blocking, synchronous way
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
 
-const textOut = `this is what we know about the avocado; ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt',textOut);
-console.log('File written!');
+// const textOut = `this is what we know about the avocado; ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt',textOut);
+// console.log('File written!');
 
 // Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-  console.log(data);
-})
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+  });
+});
+console.log('Will read file!')
