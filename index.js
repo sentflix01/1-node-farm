153ae2fb79489ebefbfdd06121c8b85d8fd9c4fb
Sintayehu <sentflix@gmail.com>
2025-03-19 10:39:07 +0300

    asynch appendfile

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const fs = require('fs')
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
     console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data2) => {
+      console.log(data2);
+    });
   });
 });
 console.log('Will read file!')
