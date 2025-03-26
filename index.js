fca7b4108bc4947fdba414f597e8a30bbacffea9
Sintayehu <sentflix@gmail.com>
2025-03-26 23:56:00 +0300

    template

--- index.js
+++ index.js
@@ -30,19 +30,33 @@ const url = require('url');
 
 // server /////////////////
 
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-Overview.Html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.Html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.Html`, 'utf-8');
+
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
   const dataObj = JSON.parse(data);
 
 const server = http.createServer((req , res) => {
   const pathName = req.url;
 
+  // Overview page
   if (pathName === '/' || pathName === '/overview') {
+
+
     res.end('This is the OVERVIEW');
+
+    // Product page
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT!');
+
+    // API 
   } else if (pathName === '/api' ) {
       res.writeHead(200, {'content-type': 'application/json'});
         res.end(data);
+
+        // Not found
   } else {
     res.writeHead(404, {
       'content-type':'text/html',
